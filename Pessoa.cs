d068b34d27963a3b44b13b690de17ecfd5f9ae3d
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-08 10:03:29 -0300

    Adicionando Super Classe

---
+++ Pessoa.cs
@@ -0,0 +1,16 @@
+namespace cadastroPessoa
+{
+    public abstract class Pessoa
+    {
+        public string nome { get; set; }
+
+        public string endereco { get; set; }
+
+        public bool enderecoComercial { get; set; }
+
+        public void pagarImposto()
+        {
+
+        }
+    }
+}
