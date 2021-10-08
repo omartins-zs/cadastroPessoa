c33c5050f70e018bb5d52ff3f214ffbd5f7bad55
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-08 10:07:51 -0300

    Adicionando SubClasses

---
+++ PessoaFisica.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cadastroPessoa
+{
+    public class PessoaFisica 
+    {
+        public string cpf { get; set; }
+
+        public DateTime dataNascimento { get; set; }
+    }
+}
---
+++ PessoaJuridica.cs
@@ -0,0 +1,11 @@
+namespace cadastroPessoa
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string cnpj { get; set; }
+
+        public string RazaoSocial { get; set; }
+
+    }
+
+}
