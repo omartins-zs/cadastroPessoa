31af1f9881d660137ee530fffa9a804930eb62da
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-10 12:00:51 -0300

    Adicionando metodo Validar Data Nascimento

--- PessoaFisica.cs
+++ PessoaFisica.cs
@@ -13,5 +13,21 @@ namespace cadastroPessoa
         {
 
         }
+        public bool ValidarDataNascimento(DateTime dataNasc)
+        {
+            DateTime dataAtual = DateTime.Today;
+
+            double anos = (dataAtual - dataNasc).TotalDays / 365;
+
+            if (anos >= 18)
+            {
+                return true;
+            }
+            // Pode ser excluido o else e apenas deixar o return false porque se nao entrar no if ja iria para o return 
+            else
+            {
+                return false;
+            }
+        }
     }
 }
