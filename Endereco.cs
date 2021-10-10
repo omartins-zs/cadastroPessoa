43dd11e5874c87ab5ab62091baaafffae8bd7b9b
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-09 23:55:56 -0300

    Adicionando Classe Endereço

---
+++ Endereco.cs
@@ -0,0 +1,14 @@
+namespace cadastroPessoa
+{
+    public class Endereco
+    {
+        public string logradouro { get; set; }
+
+        public int numero { get; set; }
+
+        public string complemento { get; set; }
+
+        public bool enderecoComercial { get; set; }
+
+    }
+}
