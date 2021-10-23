3beca82ba8e1ce5271ef52b12060b715fa6883cb
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-23 14:11:33 -0300

    Ajustando Painel de Escolha do Sistema

--- Program.cs
+++ Program.cs
@@ -41,15 +41,94 @@ namespace cadastroPessoa
                 switch (opcao)
                 {
                     case "1":
+                        // Usado apenas para chamar os objetos
+                        PessoaFisica pf = new PessoaFisica();
+
+                        // Para criar novo Endereço e Pessoa
+                        PessoaFisica novaPf = new PessoaFisica();
+                        Endereco endPf = new Endereco();
+
+                        // Instanciando Endereco
+                        endPf.logradouro = "Z";
+                        endPf.numero = 34;
+                        endPf.complemento = "Proximo ao Senai Presidente Dutra";
+                        endPf.enderecoComercial = false;
+
+                        // Instanciando Pessoa
+                        novaPf.endereco = endPf;
+                        novaPf.cpf = "32442342198";
+                        novaPf.nome = "Pessoa Fisica";
+                        novaPf.dataNascimento = new DateTime(2000, 06, 15);
+
+                        // Usando a funçao de validar data | Passo a Passo
+                        bool idadeValida = novaPf.ValidarDataNascimento(novaPf.dataNascimento);
+                        // Console.WriteLine(idadeValida);
+
+                        if (idadeValida == true)
+                        {
+                            Console.WriteLine($"Cadastro Aprovado!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Cadastro Reprovado!");
+                        }
+
                         break;
 
                     case "2":
+                        // Instancia a Pessoa Juridica e Chama Metodo
+                        PessoaJuridica pj = new PessoaJuridica();
+
+                        // Para Receber valor / Atributos
+                        PessoaJuridica novaPj = new PessoaJuridica();
+
+                        Endereco endPj = new Endereco();
+
+                        // Instanciando Endereco
+                        endPj.logradouro = "Z";
+                        endPj.numero = 34;
+                        endPj.complemento = "Proximo ao Senai Presidente Dutra";
+                        endPj.enderecoComercial = true;
+
+                        novaPj.endereco = endPj;
+                        novaPj.cnpj = "31235679230001";
+                        novaPj.RazaoSocial = "Pessoa Juridica";
+
+                        // No Pessoa Fisica guardando no bool | Exemplo a baixo realizado direto
+                        // Se for condiçao true nao precisa adiconar return true | Se for false no inicio da comparação adicionar o "!"
+                        if (pj.ValidarCNPJ(novaPj.cnpj))
+                        {
+                            Console.WriteLine($"CNPJ iNVALIDO");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"CNPJ Cadastrado com Sucesso");
+                        }
+ ;
                         break;
 
                     case "0":
+                        Console.Clear();
+                        Console.WriteLine($"Obrigado Por Utilizar nosso sistema");
+
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.BackgroundColor = ConsoleColor.White;
+
+                        Console.WriteLine($"Finalizando");
+                        Thread.Sleep(500);
+
+                        for (var adicionarDot = 0; adicionarDot < 10; adicionarDot++)
+                        {
+                            Console.Write($".");
+                            Thread.Sleep(250);
+                        }
+                        Console.ResetColor();
+
                         break;
 
                     default:
+                        Console.ResetColor();
+                        Console.WriteLine($"Opção Inválida, por favor digite uma opção válida");
                         break;
                 }
 
