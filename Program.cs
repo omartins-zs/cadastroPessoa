55cc04ccc77eacda151435fd760eec44630d101b
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-23 14:06:42 -0300

    Adicionando Opcões de Escolha do Painel

--- Program.cs
+++ Program.cs
@@ -20,7 +20,12 @@ namespace cadastroPessoa
             // Usando Function Da Barra de Carregamento | Adicionando "."
             AdicionarDot("Iniciando");
 
-            Console.WriteLine($@"
+            // Criaçao da Variavel opcao
+            string opcao;
+
+            do
+            {
+                Console.WriteLine($@"
 |================================================|
 |       Escolha uma das opções abaixo            |
 |------------------------------------------------|
@@ -30,86 +35,26 @@ namespace cadastroPessoa
 |       0 - Sair                                 |
 |================================================|
                 ");
-            // Usado apenas para chamar os objetos
-            //     PessoaFisica pf = new PessoaFisica();
 
-            //     // Para criar novo Endereço e Pessoa
-            //     PessoaFisica novaPf = new PessoaFisica();
-            //     Endereco end = new Endereco();
+                opcao = Console.ReadLine();
 
-            //     // Instanciando Endereco
-            //     end.logradouro = "Z";
-            //     end.numero = 34;
-            //     end.complemento = "Proximo ao Senai Presidente Dutra";
-            //     end.enderecoComercial = false;
+                switch (opcao)
+                {
+                    case "1":
+                        break;
 
-            //     // Instanciando Pessoa
-            //     novaPf.endereco = end;
-            //     novaPf.cpf = "32442342198";
-            //     novaPf.nome = "Pessoa Fisica";
-            //     novaPf.dataNascimento = new DateTime(2000, 06, 15);
+                    case "2":
+                        break;
 
-            //     // // Mostrar no terminal
-            //     // Console.WriteLine(novaPf.endereco.logradouro);
+                    case "0":
+                        break;
 
-            //     // // Interpolação
-            //     // Console.WriteLine($"Rua: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}");
+                    default:
+                        break;
+                }
 
-            //     // // Concatenaçao
-            //     // Console.WriteLine("Rua:" + novaPf.endereco.logradouro + ", " + novaPf.endereco.numero);
+            } while (opcao != "0");
 
-            //     // // Sem pular linha
-            //     // Console.WriteLine(novaPf.endereco.logradouro);
-
-            //     // // Mostrar varios comandos no mesmo console (Mostra como voce deixa no codigo)
-            //     // Console.WriteLine($@"Rua: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}");
-
-            //     // Usando a funçao de validar data | Passo a Passo
-            //     bool idadeValida = novaPf.ValidarDataNascimento(novaPf.dataNascimento);
-            //     // Console.WriteLine(idadeValida);
-
-            //     // // Usando a funçao de validar data | Metodo Direto
-            //     // Console.WriteLine(novaPf.ValidarDataNascimento(novaPf.dataNascimento));
-
-            //     if (idadeValida == true)
-            //     {
-            //         Console.WriteLine($"Cadastro Aprovado!");
-            //     }
-            //     else
-            //     {
-            //         Console.WriteLine($"Cadastro Reprovado!");
-            //     } 
-
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-
-            // Instancia a Pessoa Juridica e Chama Metodo
-            PessoaJuridica pj = new PessoaJuridica();
-
-            // Para Receber valor / Atributos
-            PessoaJuridica novaPj = new PessoaJuridica();
-
-            Endereco end = new Endereco();
-
-            // Instanciando Endereco
-            end.logradouro = "Z";
-            end.numero = 34;
-            end.complemento = "Proximo ao Senai Presidente Dutra";
-            end.enderecoComercial = true;
-
-            novaPj.endereco = end;
-            novaPj.cnpj = "31235623000179";
-            novaPj.RazaoSocial = "Pessoa Juridica";
-
-            // No Pessoa Fisica guardando no bool | Exemplo a baixo realizado direto
-            // Se for condiçao true nao precisa adiconar return true | Se for false no inicio da comparação adicionar o "!"
-            if (pj.ValidarCNPJ(novaPj.cnpj))
-            {
-                Console.WriteLine($"CNPJ Válido");
-            }
-            else
-            {
-                Console.WriteLine($"CNPJ iNVALIDO");
-            };
         }
         static void AdicionarDot(string textoCarregamento)
         {
