dbb609b128f25404dec1bed3d51cc19cb71c02f3
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-05 17:00:43 -0300

    Adicionando Metodo Pagar Imposto no Pessoa Juridica

--- PessoaJuridica.cs
+++ PessoaJuridica.cs
@@ -7,11 +7,21 @@ namespace cadastroPessoa
         public string RazaoSocial { get; set; }
 
         // Adicionando Polimorfismo
-        public override void pagarImposto(float salario)
+        public override double pagarImposto(float rendimento)
         {
-
+            if (rendimento <= 5000)
+            {
+                return rendimento * .06;
+            }
+            else if (rendimento > 5000 && rendimento <= 10000) // ou (rendimento <= 5000)
+            {
+                return rendimento * 0.08; // 1° Exemplo Para realizar calculo da porcentagem 
+            }
+            else
+            {
+                return (rendimento / 100) * 10; // 2° Exemplo
+            }
         }
-
         public bool ValidarCNPJ(string cnpj)
         {
             if (cnpj.Length == 14 && cnpj.Substring(cnpj.Length - 6, 4) == "0001")
