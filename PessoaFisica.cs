09246a1af19b1f61f6dc2860f6afe8669f3ef0dd
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-05 16:59:25 -0300

    Criando e Ajustando o Metodo Pagar Imposto

--- PessoaFisica.cs
+++ PessoaFisica.cs
@@ -9,9 +9,20 @@ namespace cadastroPessoa
         public DateTime dataNascimento { get; set; }
 
         // Adicionando Polimorfismo
-        public override void pagarImposto(float salario)
+        public override double pagarImposto(float rendimento)
         {
-
+            if (rendimento <= 1500)
+            {
+                return 0;
+            }
+            else if (rendimento > 1500 && rendimento <= 5.000) // ou (rendimento <= 5000)
+            {
+                return rendimento * 0.03; // 1° Exemplo Para realizar calculo da porcentagem Multiplicação
+            }
+            else
+            {
+                return (rendimento / 100) * 5; // 2 Exemplo Divisão
+            }
         }
         public bool ValidarDataNascimento(DateTime dataNasc)
         {
